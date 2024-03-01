cae181f627b980d59fc48f270fe380c4f8ed37af
Anshul Negi <anshuln168@gmail.com>
2024-03-01 22:01:52 +0530

    'learning react-router'

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Components/Navbar";
+import Home from "./Components/Home";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Home />
+      <About />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ reactRouter/src/main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Layout from "../Layout";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
