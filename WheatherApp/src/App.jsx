530e333e6d9a6b86945e2cd95bf433fb70f78bb5
Anshul Negi <anshuln168@gmail.com>
2024-03-06 21:18:23 +0530

    'app'

--- WheatherApp/src/App.jsx
+++ WheatherApp/src/App.jsx
@@ -1,14 +1,66 @@
-
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  let [city, setCity] = useState("");
+  let [wheatherData, setWheatherData] = useState({});
+
+  const api_key = `f53f917a8a58dd893be0ce33f0ea965c`;
+  let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+  const fetchwheather = () => {
+    try {
+      fetch(api)
+        .then((res) => res.json())
+        .then((data) => {
+          setWheatherData(data);
+          console.log(data);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
-    Hello 
+      <div>
+        <div>
+          <input
+            type="text"
+            value={city}
+            onChange={(e) => {
+              setCity(e.target.value);
+            }}
+          />
+        </div>
+
+        <div>
+          <button onClick={fetchwheather} style={{ margin: "20px" }}>
+            Fetch
+          </button>
+        </div>
+
+        {typeof wheatherData.main !== "undefined" ? (
+          <div>
+            <p>{wheatherData.name}</p>
+
+            <p>{Math.floor(wheatherData.main.temp - 273.15)}°C</p>
+
+            {wheatherData.weather[0].icon !== "undefined" ? (
+              <img
+                src={`https://openweathermap.org/img/wn/${wheatherData.weather[0].icon}@2x.png`}
+                alt=""
+              />
+            ) : (
+              <p>wheatherData.weather[0].icon</p>
+            )}
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </>
-      
-  )
+  );
 }
 
-export default App
+export default App;
