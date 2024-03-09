84329b642cdfde58b7a1315273a3d1a52b8e6f42
Anshul Negi <anshuln168@gmail.com>
2024-03-09 20:07:25 +0530

    ...

--- Accordian/src/App.jsx
+++ Accordian/src/App.jsx
@@ -5,20 +5,46 @@ import "./App.css";
 
 function App() {
   const [selected, setSelected] = useState(null);
+  const [multiSelection, setMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
-  const handleClick = (id) => {
+  const handleSingleSelection = (id) => {
     setSelected(id === selected ? null : id);
   };
+
+  const handleMultiSelection = (id) => {
+    const copymulti = [...multiple];
+    const current_id_cpy = copymulti.indexOf(id);
+    if (current_id_cpy === -1) copymulti.push(id);
+    else copymulti.splice(current_id_cpy, 1);
+    setMultiple(copymulti);
+  };
+  console.log(multiple);
+
   return (
     <>
       <h1>Hello </h1>
+      <button onClick={() => setMultiSelection(!multiSelection)}>
+        Multiple Selection
+      </button>
       <div>
         {data.map((dataItems) => (
-          <div key={dataItems.id} onClick={() => handleClick(dataItems.id)}>
+          <div
+            key={dataItems.id}
+            onClick={() =>
+              multiSelection
+                ? handleMultiSelection(dataItems.id)
+                : handleSingleSelection(dataItems.id)
+            }
+          >
             <h3>{dataItems.question}</h3>
             <span>+</span>
             <div>
-              {selected === dataItems.id ? <div>{dataItems.answer}</div> : null}
+              {multiSelection
+                ? multiple.indexOf(dataItems.id) !== -1 && (
+                    <div>{dataItems.answer}</div>
+                  )
+                : selected === dataItems.id && <div>{dataItems.answer}</div>}
             </div>
           </div>
         ))}
