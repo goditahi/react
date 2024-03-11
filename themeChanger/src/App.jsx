3aca0c21fbe547087ea3ed7ee9f3718140aa4a38
Anshul Negi <anshuln168@gmail.com>
2024-03-11 23:49:19 +0530

    themeChanger

---
+++ themeChanger/src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import Themebtn from "./components/Themebtn";
+import Card from "./components/Card";
+import { ThemeProvider } from "./context/theme";
+
+export default function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <Themebtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
